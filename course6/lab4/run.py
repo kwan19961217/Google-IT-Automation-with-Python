508142b8261bc55ca54bae29c1c0caf4fe2b8cfc
kwanth <59208523+kwan19961217@users.noreply.github.com>
2021-06-08 14:09:37 +0800

    update weight handling

--- course6/lab4/run.py
+++ course6/lab4/run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import os
 import requests
+import re
+
 
 url = "IP Address"
 path ="./supplier-data/description/"
@@ -10,8 +12,9 @@ for file in files:
 	if file.endwswith(".txt"):
 		with open(file) as f:
 			rows = f.readlines()
+			weight = re.match(r"[\d]+", rows[1])[0]
 			requests.post(url, files={
 				"name": rows[0],
-				"weight": rows[1],
+				"weight": weight,
 				"description": rows[2]": 
-				"image_name": file[:-3] + ".jpeg"})
+				"image_name": file[:-4] + ".jpeg"})
