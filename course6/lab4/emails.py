8b77587c20cab58364832bca39f8f8ccf401ea7e
kwanth <59208523+kwan19961217@users.noreply.github.com>
2021-06-08 22:09:46 +0800

    add emails.py prototype

---
+++ course6/lab4/emails.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from email.message import EmailMessage
+import mimetypes
+import smtplib
+
+def generate_email(sender, recipient, subject, body, attachment):
+	message = EmailMessage()
+	message["From"] = sender
+	message["To"] = recipient
+	message["Subject"] = subject
+	message.set_content(body)
+
+	mime_type, _ = mimetypes.guess_type(attachment)
+	mime_type, mime_subtype = mime_type.split('/', 1)
+	with open(attachment, 'rb') as ap:
+		message.add_attachment(ap.read(),
+								maintype=mime_type,
+								subtype=mime_subtype,
+								filename="processed.pdf")
+	return message
+
+def send_email(sender, message):
+	mail_server = smtplib.SMTP_SSL('smtp.example.com')
+	mail_server.login(sender)
+	mail_server.send_message(message)
+	mail_server.quit()
