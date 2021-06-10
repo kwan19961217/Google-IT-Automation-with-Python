a51984a92b2cf4a8fcbd784cebf30c6cf07be5dc
kwanth <59208523+kwan19961217@users.noreply.github.com>
2021-06-10 22:33:58 +0800

    update supplier_image_upload.py final

--- course6/lab4/supplier_image_upload.py
+++ course6/lab4/supplier_image_upload.py
@@ -1,12 +1,13 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 import requests
 import os
 
 url = "IP Address"
-path = "./supplier/images/"
+path = "./supplier-data/images/"
 
 files = os.listdir(path)
 for file in files:
 	if file.endswith(".jpeg"):
+		print(file)
 		with open(path + file, "rb") as f:
 			requests.post(url, files={"file": f})
