d32961e9583db8e4cfd6d063ffec5baac9957e24
kwanth <59208523+kwan19961217@users.noreply.github.com>
2021-06-10 23:13:08 +0800

    report_email.py final

--- course6/lab4/report_email.py
+++ course6/lab4/report_email.py
@@ -7,18 +7,18 @@ import emails
 
 if __name__ == "__main__":
 	path = "./supplier-data/descriptions/"
+	report_path = "/tmp/processed.pdf"
 	files = os.listdir(path)
 	content = ""
-	today = datetime.datetime.today()
-	report_path = "/tmp/processed.pdf"
+	today = str(datetime.date.today())
 
 	for file in files:
 		with open(path + file) as f:
 			rows = f.readlines()
-			temp = f"name: {rows[0]}\nweight: {rows[1]}\n"
+			temp = f"<br/>name: {rows[0]}<br/>weight: {rows[1]}<br/><br/>"
 			content += temp
 
-	reports.generate_report(report_path, f"Processed Update on {today}", content
+	reports.generate_report(report_path, f"Processed Update on {today}", content)
 	message = emails.generate_email("automation@example.com", "username@example.com", 
 							"Upload Completed - Online Fruit Store", 
 							"All fruits are uploaded to our website successfully. A detailed list is attached to this email.", 
