e535235f31c9498621fa5a301dfbee1f4c2743a9
kwanth <59208523+kwan19961217@users.noreply.github.com>
2021-06-10 23:12:54 +0800

    emails.py final

--- course6/lab4/emails.py
+++ course6/lab4/emails.py
@@ -3,24 +3,24 @@ from email.message import EmailMessage
 import mimetypes
 import smtplib
 
-def generate_email(sender, recipient, subject, body, attachment):
+def generate_email(sender, recipient, subject, body, attachment=None):
 	message = EmailMessage()
 	message["From"] = sender
 	message["To"] = recipient
 	message["Subject"] = subject
 	message.set_content(body)
 
-	mime_type, _ = mimetypes.guess_type(attachment)
-	mime_type, mime_subtype = mime_type.split('/', 1)
-	with open(attachment, 'rb') as ap:
-		message.add_attachment(ap.read(),
-								maintype=mime_type,
-								subtype=mime_subtype,
-								filename="processed.pdf")
+	if attachment is not None:
+		mime_type, _ = mimetypes.guess_type(attachment)
+		mime_type, mime_subtype = mime_type.split('/', 1)
+		with open(attachment, 'rb') as ap:
+			message.add_attachment(ap.read(),
+									maintype=mime_type,
+									subtype=mime_subtype,
+									filename="processed.pdf")
 	return message
 
 def send_email(sender, message):
-	mail_server = smtplib.SMTP_SSL('smtp.example.com')
-	mail_server.login(sender)
+	mail_server = smtplib.SMTP('localhost')
 	mail_server.send_message(message)
 	mail_server.quit()
