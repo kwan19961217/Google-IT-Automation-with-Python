31095077ea8c4a4819bfb0694eca2fe17394caa1
kwanth <59208523+kwan19961217@users.noreply.github.com>
2021-06-10 22:23:51 +0800

    update changeImage.py final

--- course6/lab4/changeImage.py
+++ course6/lab4/changeImage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/evn python3
+#!/usr/bin/env python3
 
 from PIL import Image
 import os
@@ -9,7 +9,8 @@ path = "./supplier-data/images/"
 files = os.listdir(path)
 for file in files:
 	try:
-		im = Image.open(path + image).convert("RGB")
-		im.resize((600,400)).save(path + image, "jpeg")
+		f, e = os.path.splitext(file)
+		im = Image.open(path + file).convert("RGB")
+		im.resize((600,400)).save(path + f +".jpeg")
 	except:
 		print("Not an image file.")
